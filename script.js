695810560f7a88639b04867e9e8a04695546b35a
Siddharth Mal <smal@continuumcloud.com>
2021-08-21 10:27:16 -0400

    done

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Graduate Student", "Designer", "Freelancer","Soccer Enthusiast"],
+        strings: ["MAKE NEW MEMORIES","CHILL", " HAVE AN ADVENTURE", "EXPLORE MANALI", "HAVE A GREAT TIME","MEET NEW PEOPLE"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
